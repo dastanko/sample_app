fe0db16f130a30a7365b3733ec29ccea0dc7cd64
Dastan <dastanko089@gmail.com>
2011-11-24 00:20:30 +0600

    Added a title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Этот метод показывает страницу умно
+  def title
+    basic_title = "Ruby on Rails Tutorial Sample App"
+
+    if @title.nil?
+      basic_title
+    else
+      "#{basic_title} | #{@title}"
+    end
+  end
 end

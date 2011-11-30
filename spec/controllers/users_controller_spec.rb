9f9411225774ae9f41a16b96d92398fc591d13bb
Dastan <dastanko089@gmail.com>
2011-11-30 22:18:56 +0600

    Finished user sign up

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -52,4 +52,55 @@ describe UsersController do
     end
   end
 
+  describe "POST 'create'" do
+
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => ""}
+      end
+
+      it "should have a right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render a 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+    end
+
+    describe "success" do
+      before(:each) do
+        @attr = {:name => "Dastan Kojomuratov", :email                 => "dastanko_89@namba.kg",
+                                                :password              => "dastan",
+                                                :password_confirmation => "dastan"}
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+
+      it "should redirect to a user 'show' page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+    end
+
+  end
+
+
 end

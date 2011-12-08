c19e944cde715144dc74d78531e07cfc0e6a501a
Dastan <dastanko089@gmail.com>
2011-12-08 22:05:14 +0600

    Finished microposts

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -58,4 +58,35 @@ describe MicropostsController do
     end
   end
 
+  describe "DELETE 'destroy'" do
+    describe "for unauthorized user" do
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        @micropost = Factory(:micropost, :user => @user)
+        test_sign_in(wrong_user)
+      end
+
+      it "should deny access"  do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+    end
+
+    describe "for authorized user" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @micropost = Factory(:micropost, :user => @user)
+      end
+
+      it "should destroy a micropost" do
+        lambda do
+          delete :destroy, :id => @micropost
+          response.should redirect_to(root_path)
+          flash[:success].should =~ /deleted/i
+        end.should change(Micropost, :count).by(-1)
+      end
+    end
+  end
+
 end

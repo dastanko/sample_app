9e5dcee75fbaf45d6716ba9e88d2b0a79458424f
Dastan <dastanko089@gmail.com>
2011-12-08 16:51:31 +0600

    Added a Micropost model with User associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |add|
-      valid_email_user =  User.new(@attr.merge(:email => add))
+      valid_email_user = User.new(@attr.merge(:email => add))
       valid_email_user.should be_valid
     end
   end
@@ -165,9 +165,33 @@ describe User do
       @user.should be_admin
     end
   end
+
+  describe "microposts associations" do
+    before(:each) do
+      @user = User.create(@attr)
+      @mp1 = Factory(:micropost, :user => @user, :created_at => 1.day.ago)
+      @mp2 = Factory(:micropost, :user => @user, :created_at => 1.hour.ago)
+    end
+
+    it "should have a microposts attribute" do
+      @user.should respond_to(:microposts)
+    end
+
+    it "should have the right microposts inn right order" do
+      @user.microposts.should == [@mp2, @mp1]
+    end
+
+    it "should destroy associated microposts" do
+      @user.destroy
+      [@mp1, @mp2].each do |micropost|
+        lambda do
+          Micropost.find(micropost)
+        end.should raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+  end
 end
-
-
 
 
 # == Schema Information
